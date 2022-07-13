2be38e27ae1af7575b6964729b75a1df7a695a8f
leanhtuan2703 <pieyep2703@gmail.com>
2022-07-13 10:42:07 +0700

    Make netflix use ReactJS and deploy with Firebase

---
+++ src/components/login/Login.js
@@ -0,0 +1,18 @@
+import Nav from '../nav/Nav';
+import LoginForm from './LoginForm';
+
+
+const Login = () => {
+    return (
+        <div className="main">
+            {/* Nav */}
+            <Nav />
+            {/* End Nav */}
+            {/* Login Form */}
+            <LoginForm />
+            {/* End Login Form */}
+        </div>
+    );
+}
+
+export default Login
